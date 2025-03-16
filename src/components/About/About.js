e02a942407e42a5da4dc670642415f7e80b2c830
sahilcodes2002 <codessahil@gmail.com>
2025-03-17 01:50:12 +0530

    copy button to copy mail

---
+++ src/components/About/About.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Sahil Kumar Sinha</strong>. I'm a passionate B.Tech undergraduate currently in my third year at SRM Institute of Science and Technology. With a stellar academic record (CGPA: 9.54) and an unyielding enthusiasm for software development, I specialize in crafting innovative solutions to complex problems. My love for data structures and algorithms and competitive programing fuels my journey as a developer who thrives on the challenges of building robust, scalable systems.
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            I aspire to carve a dynamic career as a Software Development Engineer, contributing to impactful projects and tackling intricate puzzles with cutting-edge technology. Looking ahead, my vision is to lead and empower teams at top tech companies, working on transformative solutions that push the boundaries of innovation.
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+            What drives me? It's the belief in the power of development and coding to solve real-world issues, uplift communities, and create lasting value. I am a dedicated and relentless learner who embraces every opportunity to expand my skill set and stay ahead in the ever-evolving tech landscape. Beyond coding, I find fulfillment in turning ideas into meaningful action and collaborating with like-minded innovators.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,136 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/habito.png",
+    title: "Habito",
+    description:
+      "Habito is a versatile task management platform designed to help users track progress, manage projects, save notes, and collaborate effectively, all within a secure and user-friendly interface.",
+    tech_stack: ["React", "Cloudflare", "HonoJs", "TailwindCSS", "Postgresql", "NodeJs"],
+    github_url: "https://github.com/sahilcodes2002/Habito",
+    demo_url: "https://sahilcodes2002.github.io/Habito/#/",
+  },
+  {
+    img: "/OPPORTUNITREE.png",
+    title: "Opportunitree",
+    description:
+      "OPPORTUNITREE empowers high school students with AI assisted personalized guidance for colleges, skills, extracurriculars, and opportunities to achieve their dreams.",
+    tech_stack: ["OpenAi" ,"React", "Cloudflare", "ExpressJs", "TailwindCSS", "Postgresql", "NodeJs"],
+    github_url: "https://github.com/sahilcodes2002/opportunitree",
+    demo_url: "https://sahilcodes2002.github.io/opportunitree/#/",
+  },
+  {
+    img: "/autonote.png",
+    title: "AutoNote",
+    description:
+      "Effortlessly make notes from any website by just selecting text from the website",
+      tech_stack: ["ReactJs", "Webpack", "TailwindCSS", "TypeScript","Styled-components", "ExpressJs", "Chrome Extension APIs", "Postgress", "OpenAi"],
+    github_url: "https://github.com/sahilcodes2002/AutoNote",
+    demo_url: ""
+  },
+  {
+    img: "/tabnest.png",
+    title: "TabNest",
+    description:
+      "TabNest: Seamlessly manage your browser tabs and history across devices and browsers for effortless organization.",
+    tech_stack: ["ReactJs", "Webpack", "TailwindCSS", "TypeScript","Styled-components", "ExpressJs", "Chrome Extension APIs", "Postgress"],
+    github_url: "https://github.com/sahilcodes2002/TabNest-Extension",
+    demo_url: "",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "/recoil.jpg",
+    name: "Recoil",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vite/vite-original.svg",
+    name: "Vite",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+    name: "C++",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vercel/vercel-original.svg",
+    name: "Vercel",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+    name: "AWS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cloudflareworkers/cloudflareworkers-original-wordmark.svg",
+    name: "Cloudflare",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original.svg",
+    name: "postgresql",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg",
+    name: "Docker",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+    name: "tailwindcss",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
